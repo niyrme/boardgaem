9f22ed9b69d27a2f36cd23dd335cf0d36a898ef1
niyrme <niyrme@niyrme.xyz>
2021-10-05 13:13:05 +0200

    Add game select

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
-use gaems::Game;
+use std::io;
 
-use crate::gaems::yahtzee::Yahtzee;
+use crate::gaems::{yahtzee::Yahtzee, Game};
 
 pub mod gaems;
 
-fn main() { Yahtzee::new().start(); }
+fn main() {
+	println!("Select a game to play");
+	println!(" 0. Exit program");
+	println!(" 1. Yahtzee");
+
+	let mut choice = String::new();
+	io::stdin().read_line(&mut choice).expect("failed to read line");
+
+	let choice: u16 = choice.trim().parse().expect("failed to parse line");
+
+	match choice {
+		0 => return,
+		1 => Yahtzee::new().start(),
+		_ => println!("Game {} does not exist", choice),
+	}
+}
